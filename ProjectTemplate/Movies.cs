520aff5cdc323ff04c999360ae8b06e2d3cab7ff
Hannah Byrne <S00189764@mail.itsligo.ie>
2020-01-29 15:27:27 +0000

    Add project files.

---
+++ ProjectTemplate/Movies.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTemplate
+{
+    class Movies
+    {
+        public string MovieName { get; set; }
+        public string MovieGenre { get; set; }
+        public string MovieDescription { get; set; }
+
+        public Movies(string movieName, string movieGenre, string movieDescription)
+        {
+            MovieName = movieName;
+            MovieGenre = movieGenre;
+            MovieDescription = movieDescription;
+        }
+
+        public override string ToString()
+        {
+            return $"{MovieName}";
+        }
+    }
+}
