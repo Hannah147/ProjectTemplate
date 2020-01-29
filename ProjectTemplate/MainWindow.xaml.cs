498e7e8f0ee0091854ae8133416cb99fd01d01df
Hannah Byrne <S00189764@mail.itsligo.ie>
2020-01-29 15:53:27 +0000

    Read in from text boxes, and added to a list

--- ProjectTemplate/MainWindow.xaml.cs
+++ ProjectTemplate/MainWindow.xaml.cs
@@ -20,9 +20,76 @@ namespace ProjectTemplate
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Movies> AllMovies = new List<Movies>();
+        List<Movies> WatchedMovies = new List<Movies>();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string movieName = tbxMovieName.Text;
+            string movieGenre = cbxGenre.SelectedItem.ToString();
+            string movieDescription = tbxDescription.Text;
+
+            /*Movies[] MoviesAll = new Movies[100];
+
+            for (int counter = 0; counter < 5; counter++)
+            {
+                MoviesAll[counter] = new Movies(movieName, movieGenre, movieDescription);
+                AllMovies.Add(MoviesAll[counter]);
+            }*/
+
+            Movies movie1 = new Movies(movieName, movieGenre, movieDescription);
+
+            AllMovies.Add(movie1);
+
+            lbxWatchlist.ItemsSource = AllMovies;
+
+            tbxMovieName.Clear();
+            tbxDescription.Clear();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string[] Genres = { "Horror", "Comedy", "Action", "Romance", "Superhero", "Drama" };
+            cbxGenre.ItemsSource = Genres;
+            cbxGenre.SelectedIndex = 0;
+
+            cbxGenreSort.ItemsSource = Genres;
+            cbxGenreSort.SelectedIndex = 0;
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Movies selectedMovie = lbxWatchlist.SelectedItem as Movies;
+
+            if(selectedMovie != null)
+            {
+                AllMovies.Remove(selectedMovie);
+                WatchedMovies.Add(selectedMovie);
+
+                RefreshScreen();
+            }
+        }
+
+        private void RefreshScreen()
+        {
+            lbxWatchlist.ItemsSource = null;
+            lbxWatchlist.ItemsSource = AllMovies;
+
+            lbxWatched.ItemsSource = null;
+            lbxWatched.ItemsSource = WatchedMovies;
+        }
+
+        private void lbxWatchlist_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Movies selectMovie = lbxWatchlist.SelectedItem as Movies;
+            if(selectMovie != null)
+            {
+                tblkDescription.Text = selectMovie.MovieDescription;
+            }
         }
     }
 }
