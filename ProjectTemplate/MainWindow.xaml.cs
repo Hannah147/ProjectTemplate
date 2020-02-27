c2a76d88d5fc7d31c6dc6708088139918b350ad6
Hannah Byrne <S00189764@mail.itsligo.ie>
2020-02-27 12:46:20 +0000

    Filtered by genre and rating, not fully working yet

--- ProjectTemplate/MainWindow.xaml.cs
+++ ProjectTemplate/MainWindow.xaml.cs
@@ -21,13 +21,18 @@ namespace ProjectTemplate
     public partial class MainWindow : Window
     {
         List<Movies> AllMovies = new List<Movies>();
+        List<Movies> AllMoviesRated = new List<Movies>();
         List<Movies> WatchedMovies = new List<Movies>();
+        List<Movies> FilterWatchlist = new List<Movies>();
+        List<Movies> FilterWatched = new List<Movies>();
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        // Add Movie to Movie List, calling class
+        private void btnAddWatch_Click(object sender, RoutedEventArgs e)
         {
             string movieName = tbxMovieName.Text;
             string movieGenre = cbxGenre.SelectedItem.ToString();
@@ -45,36 +50,31 @@ namespace ProjectTemplate
             tbxDescription.Clear();
         }
 
+        // Window loaded, sorting combo boxes
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            string[] Genres = { "Horror", "Comedy", "Action", "Romance", "Superhero", "Drama" };
+            string[] Genres = { "Select..." ,"Horror", "Comedy", "Action", "Romance", "Superhero", "Drama" };
             cbxGenre.ItemsSource = Genres;
             cbxGenre.SelectedIndex = 0;
 
             cbxGenreSort.ItemsSource = Genres;
             cbxGenreSort.SelectedIndex = 0;
 
-            string[] Ratings = { "1 Star", "2 Stars", "3 Stars", "4 Stars", "5 Stars" };
+            string[] Ratings = { "Select...", "1 Star", "2 Stars", "3 Stars", "4 Stars", "5 Stars" };
             cbxAddRating.ItemsSource = Ratings;
+            cbxAddRating.SelectedIndex = 0;
 
             cbxRatingFilter.ItemsSource = Ratings;
+            cbxRatingFilter.SelectedIndex = 0;
         }
 
+        // Add movie to watched list from watchlist
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
             Movies selectedMovie = lbxWatchlist.SelectedItem as Movies;
 
             if(selectedMovie != null)
             {
-                /*string movieName = tbxMovieName.Text;
-                string movieGenre = cbxGenre.SelectedItem.ToString();
-                string movieDescription = tbxDescription.Text;
-                string movieRating = cbxGenre.SelectedItem.ToString();
-
-                MoviesWatched movieWatched = new MoviesWatched(movieName, movieGenre, movieDescription, movieRating);
-
-                WatchedMovies.Add(movieWatched);*/
-
                 if (cbxAddRating.SelectedItem != null)
                 {
                     selectedMovie.MovieRating = cbxAddRating.SelectedItem.ToString();
@@ -90,6 +90,7 @@ namespace ProjectTemplate
             }
         }
 
+        // Refresh the screen
         private void RefreshScreen()
         {
             lbxWatchlist.ItemsSource = null;
@@ -99,6 +100,7 @@ namespace ProjectTemplate
             lbxWatched.ItemsSource = WatchedMovies;
         }
 
+        // Display movie description under watchlist on selected movie
         private void lbxWatchlist_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Movies selectMovie = lbxWatchlist.SelectedItem as Movies;
@@ -108,12 +110,74 @@ namespace ProjectTemplate
             }
         }
 
+        // Add movie to watched list with rating on selected movie
         private void lbxWatched_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Movies selectMovie = lbxWatched.SelectedItem as Movies;
             if (selectMovie != null)
             {
                 tblkRating.Text = selectMovie.MovieRating;
+                AllMoviesRated.Add(selectMovie);
+            }
+        }
+
+        // Sort by genre when selection changes on watchlist
+        private void CbxGenreSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedGenre = cbxGenreSort.SelectedItem as string;
+            //Movies selectMovie = lbxWatchlist.SelectedItem as Movies;
+
+            //FilterWatchlist.Clear();
+
+            foreach (Movies movie in AllMovies)
+            {
+                if (movie.MovieGenre == selectedGenre)
+                {
+                    //FilterWatchlist.Clear();
+                    FilterWatchlist.Add(movie);
+                    lbxWatchlist.ItemsSource = null;
+                    lbxWatchlist.ItemsSource = FilterWatchlist;
+
+                }
+
+                else if(movie.MovieGenre != selectedGenre)
+                {
+                    FilterWatchlist.Clear();
+                    lbxWatchlist.ItemsSource = null;
+                    lbxWatchlist.ItemsSource = FilterWatchlist;
+                }
+
+                else
+                {
+                    lbxWatchlist.ItemsSource = FilterWatchlist;
+                }
+            }
+        }
+
+        // Sort by rating on watched list
+        private void CbxRatingFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedRating = cbxRatingFilter.SelectedItem as string;
+            //Movies selectMovie = lbxWatchlist.SelectedItem as Movies;
+
+            FilterWatched.Clear();
+
+            foreach (Movies movie in AllMoviesRated)
+            {
+                if (movie.MovieRating == selectedRating)
+                {
+                    FilterWatched.Add(movie);
+                    lbxWatched.ItemsSource = null;
+                    lbxWatched.ItemsSource = FilterWatched;
+
+                }
+
+                else if (movie.MovieRating != selectedRating)
+                {
+                    FilterWatched.Clear();
+                    lbxWatched.ItemsSource = null;
+                    lbxWatched.ItemsSource = FilterWatched;
+                }
             }
         }
     }
